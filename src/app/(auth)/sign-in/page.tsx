a929720b7d2c97eed9ee57d8376bde01657e5979
Utkarsh Singh <utkarshsinghcs@gmail.com>
2023-07-05 01:28:16 +0530

    WIP: Authorization

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,13 +1,16 @@
+import SignIn from '@/components/SignIn'
 import { buttonVariants } from '@/components/ui/Button'
 import { cn } from '@/lib/utils'
-import { Link } from 'lucide-react'
+import Link from 'next/link'
+
 import { FC } from 'react'
 
 
 const page: FC = () => {
   return <div className='absolute inset-0'>
     <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
-        <Link href='/' className={cn(buttonVariants({variant:'ghost'}))}>Sign In </Link>
+        <Link href='/' className={cn(buttonVariants({variant:'ghost'}), ' self-start -mt-20')}>Home</Link>
+        <SignIn/>
     </div>
   </div>
 }
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { cn } from '@/lib/utils'
+import { FC, useState } from 'react'
+import { Button } from './ui/Button'
+import {signIn} from 'next-auth/react'
+import { Icons } from './ui/Icons'
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement>{
+  
+}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({className, ...props}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const loginWithGoogle = async() =>{
+        setIsLoading(true)
+        try{
+            await signIn('google')
+        }catch(error){
+
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+  return <div className={cn('flex justify-center', className)}{...props}>
+    <Button size='sm' className='w-full' onClick={loginWithGoogle} isLoading={isLoading} >{isLoading?null:<Icons.google className='h-4 w-4 mr-2'/>}Google</Button>
+  </div>
+}
+
+export default UserAuthForm
