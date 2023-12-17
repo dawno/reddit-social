07b997b59fa422dfcf720bdf5bd3c9a2700c5664
Utkarsh Singh <utkarshsinghcs@gmail.com>
2023-12-18 02:30:51 +0530

    Completed SignIn/SignUp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,15 +14,21 @@ const inter = Inter({subsets:['latin']})
 
 export default function RootLayout({
   children,
+  authModal
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html lang='en'
     className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        {/* @ts-expect-error of server component*/}
         <Navbar/>
-      <div className='container max-w-7xl mx-auto h-full pt-12'>{children}
+
+        {authModal}
+      <div className='container max-w-7xl mx-auto h-full pt-12'>
+        {children}
       </div>
       <Toaster/>
       </body>
